a9e44e0d85b0e3223bf9c7bfd4f858be9a901c36
kneves <55398273+kneves1@users.noreply.github.com>
2020-02-29 22:35:32 -0500

    Input element created when plan div clicked, input saved to local storage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 05-Work-Day-Scheduler
+
+
+## Purpose
+
+The purpose of this project was to create a work day schedule application that stores events that are added by the hour. This application also features efficient local storage management and time-tracking functionality.
+
+## Features
+- Responsive design
+- Dynamic JavaScript and HTML
+- Local storage management 
+- jQuery, Moment.js libraries utilized
+
+
+
+## Insights
+While completing this project, I learned a lot about how to use the Moment.js library and its many different uses in JavaScript. I also developed a deeper understanding of jQuery and its ability to tighten up my code without sacrificing functionality or readability. Additionally, I used JavaScript events to create a dynamic page, and client-side storage to save the events that the client inputs. 
--- script.js
+++ script.js
@@ -8,51 +8,54 @@ $(document).ready(function(){
 
     //Creates new row with time, plan, and save
     for(let i = 0; i < 9; i++){
-         let newRow = $("<div></div>").appendTo("main").attr("class","row").css("margin", "5px"); 
-            let newSpace = $("<div></div>").appendTo(newRow).attr("class", "col-1");
-            let newTime = $("<div></div>").appendTo(newRow).attr("class", "col-2 btn btn-secondary " + [i + 9]).text((i +9) + ":00");
-            let newPlan = $("<div></div>").appendTo(newRow).attr("class", "col-6 btn planIndex" + [i]);
-            let newSave = $("<div></div>").appendTo(newRow).attr("class", "col-2 btn btn-info"); //no "saveIndex" bc save btn is same for every row
-            $('<img src="save-icon.svg">').appendTo(newSave);  //Save icon
-            newSpace;
-            let timeBlock = (i + 9);
-            
-            if (currentTime < timeBlock){
-                $(".planIndex" + [i]).addClass("btn-success");
-            }
+        let newRow = $("<div></div>").appendTo("main").attr("class","row").css("margin", "5px"); 
+        let newSpace = $("<div></div>").appendTo(newRow).attr("class", "col-1");
+        let newTime = $("<div></div>").appendTo(newRow).attr("class", "col-2 btn btn-secondary " + [i + 9]).text((i +9) + ":00");
+        let newPlan = $("<div></div>").appendTo(newRow).attr("class", "col-6 btn planIndex" + [i]);
+        // let newInput = $("<input class=input>").appendTo(newPlan);
+        let newSave = $("<div></div>").appendTo(newRow).attr("class", "col-2 btn btn-info"); //no "saveIndex" bc save btn is same for every row
+        $('<img src="save-icon.svg">').appendTo(newSave);  //Save icon
+        newSpace;
+        let timeBlock = (i + 9);
+        
+        if (currentTime < timeBlock){
+            $(".planIndex" + [i]).addClass("btn-success");
+        }
 
-            if (currentTime > timeBlock){
-                $(".planIndex" + [i]).addClass("btn-light");
-            }
-            
-            if (currentTime == timeBlock){
-                $(".planIndex" + [i]).addClass("btn-warning");
+        if (currentTime > timeBlock){
+            $(".planIndex" + [i]).addClass("btn-light");
+        }
+        
+        if (currentTime == timeBlock){
+            $(".planIndex" + [i]).addClass("btn-warning");
 
-            }
-    }
+        }
 
-
-
-
-
-
-
-
-
-    //On.click for PLAN
-    let planClick = $(".planIndex" + [i]).click(function(){
-        $("<input>").appendTo(planClick);
-
-    });
-
-
+        let planObj = newPlan.get(0);
+        let stored = localStorage.getItem(planObj.classList[2]);
+        if (stored) {
+            let inputObj = $("<input></input>").attr("class", "input" + [i]).appendTo(planObj);
+            inputObj.val(stored);
+        }
+        else {
+            //On.click for PLAN
+            $(".planIndex" + [i]).one("click", function(){
+                $("<input></input>").attr("class", "input" + [i]).appendTo(this);
+            });
+        }  
+    }   
 
 
     //On.click for SAVE
     let saveClick = $(".btn-info").click(function(){
-        // localStorage.setItem("plan", plan);
-
-
+        let planButton = this.parentElement.children[2];
+        if (planButton.hasChildNodes()) {
+            let planInput = planButton.children[0];
+            localStorage.setItem(planButton.classList[2], planInput.value);
+        }
+        else {
+            alert("No plan exists in that slot!");
+        }
     });
 
 });
